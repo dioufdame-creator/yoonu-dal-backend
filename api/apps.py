bb6a3d9bf629af02a48ec88f74003abb40a2d011
dioufdame-creator <dioufdame@gmail.com>
2026-04-06 15:41:06 +0000

    Update apps.py

--- api/apps.py
+++ api/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-def ready(self):
-    import api.signals
+    
+    def ready(self):  # ✅ 4 espaces d'indentation
+        import api.signals  # ✅ 8 espaces
