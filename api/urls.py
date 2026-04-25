4bc27b26dd7c9b777e75401d12462dc6c0376d27
dioufdame-creator <dioufdame@gmail.com>
2026-04-25 14:40:56 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -119,5 +119,5 @@ urlpatterns = [
     path('tontines/<int:tontine_id>/activity/', views.tontine_activity_feed, name='tontine_activity'),
     path('tontines/<int:tontine_id>/manage-order/', views.tontine_manage_order, name='tontine_manage_order'),
     path('tontine-contributions/<int:contribution_id>/validate/', views.validate_contribution, name='validate_contribution'),
-    
+    path('tontines/<int:tontine_id>/contributions/pending/', views.tontine_pending_contributions, name='tontine_pending_contributions'),
 ]
