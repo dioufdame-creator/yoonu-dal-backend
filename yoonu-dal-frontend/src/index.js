7c6b84c65f043417df3cba7b7b2af4b2100c3012
dioufdame-creator <dioufdame@gmail.com>
2026-05-17 09:31:49 +0000

    Update index.js

--- yoonu-dal-frontend/src/index.js
+++ yoonu-dal-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { register } from './serviceWorkerRegistration';
+import * as sw from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +13,7 @@ root.render(
 );
 
 // Activer le Service Worker PWA (mode hors ligne + notifications push)
-register();
+sw.register();
 
 // Mesure de performance (optionnel)
 reportWebVitals();
