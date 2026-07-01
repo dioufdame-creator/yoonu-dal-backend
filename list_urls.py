c55019a37c4ba664bcd431f37127ed3be673acae
dioufdame-creator <dioufdame@gmail.com>
2026-07-01 17:04:30 +0000

    Update list_urls.py

--- list_urls.py
+++ list_urls.py
@@ -9,9 +9,7 @@ django.setup()
 # Importer les modules nécessaires
 from django.urls import get_resolver
 from django.core.management import call_command
-from api.views_admin import admin_dashboard
 
 # Afficher toutes les URLs de l'application
 print("Liste des URLs:")
-path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
 call_command('show_urls')
